3916e4c6463ada66576203bd4402383e08662e52
saulzhao <zenyaretyre@gmail.com>
2026-01-18 01:22:54 +0800

    fix rss sitemap base url

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,6 +1,18 @@
 export const prerender = true;
 
-export const GET = async ({ site }) => {
+function xmlEscape(s) {
+  return String(s ?? "")
+    .replaceAll("&", "&amp;")
+    .replaceAll("<", "&lt;")
+    .replaceAll(">", "&gt;")
+    .replaceAll('"', "&quot;")
+    .replaceAll("'", "&apos;");
+}
+
+export async function GET() {
+  // 优先用 astro.config.mjs 的 site（import.meta.env.SITE），没有就用你的域名兜底
+  const base = (import.meta.env.SITE || "https://tireofmoto.com/").replace(/\/?$/, "/");
+
   const modules = import.meta.glob("../content/posts/*.md", { eager: true });
 
   const posts = Object.values(modules)
@@ -13,27 +25,31 @@ export const GET = async ({ site }) => {
     .sort((a, b) => (b.pubDate > a.pubDate ? 1 : -1));
 
   const items = posts
-    .map(
-      (p) => `
+    .map((p) => {
+      const link = `${base}posts/${p.slug}/`;
+      const pub = new Date(`${p.pubDate}T00:00:00Z`).toUTCString();
+      return `
   <item>
-    <title><![CDATA[${p.title}]]></title>
-    <link>${site}posts/${p.slug}</link>
-    <guid>${site}posts/${p.slug}</guid>
-    <pubDate>${new Date(p.pubDate + "T00:00:00Z").toUTCString()}</pubDate>
-    <description><![CDATA[${p.description}]]></description>
-  </item>`
-    )
+    <title>${xmlEscape(p.title)}</title>
+    <link>${xmlEscape(link)}</link>
+    <guid>${xmlEscape(link)}</guid>
+    <pubDate>${xmlEscape(pub)}</pubDate>
+    <description>${xmlEscape(p.description)}</description>
+  </item>`;
+    })
     .join("");
 
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
 <rss version="2.0">
 <channel>
-  <title>Tire of Moto</title>
-  <link>${site}</link>
-  <description>Motorcycle tires & inner tubes blog</description>
+  <title>${xmlEscape("Tire of Moto")}</title>
+  <link>${xmlEscape(base)}</link>
+  <description>${xmlEscape("Motorcycle tires & inner tubes blog")}</description>
   ${items}
 </channel>
 </rss>`;
 
-  return new Response(xml, { headers: { "Content-Type": "application/xml; charset=utf-8" } });
-};
+  return new Response(xml, {
+    headers: { "Content-Type": "application/xml; charset=utf-8" },
+  });
+}
--- src/pages/sitemap.xml.js
+++ src/pages/sitemap.xml.js
@@ -1,20 +1,27 @@
 export const prerender = true;
 
-export const GET = async ({ site }) => {
+export async function GET() {
+  const base = (import.meta.env.SITE || "https://tireofmoto.com/").replace(/\/?$/, "/");
+
   const modules = import.meta.glob("../content/posts/*.md", { eager: true });
 
-  const urls = Object.values(modules).map((m) => {
+  const postUrls = Object.values(modules).map((m) => {
     const slug = m.file.split("/").pop().replace(".md", "");
-    return `${site}posts/${slug}`;
+    return `${base}posts/${slug}/`;
   });
+
+  const urls = [
+    base,
+    `${base}about/`,
+    ...postUrls,
+  ];
 
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-  <url><loc>${site}</loc></url>
-  <url><loc>${site}about</loc></url>
-  <url><loc>${site}tags</loc></url>
-  ${urls.map((u) => `<url><loc>${u}</loc></url>`).join("")}
+${urls.map((u) => `  <url><loc>${u}</loc></url>`).join("\n")}
 </urlset>`;
 
-  return new Response(xml, { headers: { "Content-Type": "application/xml; charset=utf-8" } });
-};
+  return new Response(xml, {
+    headers: { "Content-Type": "application/xml; charset=utf-8" },
+  });
+}
